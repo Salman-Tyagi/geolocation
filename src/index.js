0d4707c30fbdd1315f6b2e5b06cc1fb89ce775c3
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-05 12:15:33 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import reactDOM from "react-dom/client";
+import App from "./App";
+
+reactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
